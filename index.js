b1ab356671d0b4d83bf6a99d88ad9d814b2b3330
julio <julioroja987@gmail.com>
2022-03-01 20:38:13 -0300

    Se creo servidor

--- index.js
+++ index.js
@@ -1,57 +1,5 @@
-/**
- * Servidor clasico de NODE
- */
-
-// const http = require("http"); // import http from "http" -- Forma nueva
-// const port = 5000;
-
-// const server = http.createServer((req, res) => {
-//     res.end("Hola");
-// });
-
-// server.listen(port, () => {
-//     console.log("Entraron al server ❤");
-// });
-
-/**
- * Servidor con Express
- */
-const fs = require("fs");
 const express = require("express");
 const app = express();
 const port = 3000;
-
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-
-// Peticion get y llevar los datos del form con query
-// app.get("/formulario", (req, res) => {
-//     console.log(req.query);
-//     res.send("Gracias " + req.query.nombre + ", Formulario enviado...");
-// });
-
-// Peticion post
-app.post("/formulario", (req, res) => {
-    // console.log(req.query); // para post no se usa query sino body por que la info viaja en el cuerpo del mensaje
-    // console.log(req.body);
-    const { nombre, apellido } = req.body;
-    if (!nombre || !apellido) {
-        return res.redirect("/error.html");
-    }
-    fs.writeFile(`archivos/${nombre}.txt`, apellido, (error) => {
-        if (error) {
-            return res.send("Fallo al crear el archivo");
-        }
-        res.download(__dirname + `/archivos/${nombre}.txt`);
-    });
-});
-
-app.get("/", (req, res) => {
-    res.send("Hola WEB");
-});
-
-app.get("/inicio", (req, res) => {
-    res.send("PAGINA DE INICIO");
-});
 
 app.listen(port, () => console.log("escuchando"));
